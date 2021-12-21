1dec3112c79ce8ee0476c5fe1dcfcf6a00c3a2e6
dmitr <dmitrykhilko@mail.ru>
2021-12-21 16:50:14 +0300

    Добавил в LoginTest описание того, как несколько наборов тестовых данных добавить

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -10,8 +10,8 @@ public class LoginTest extends BaseTest {
     // Вопрос: Что делать, если в тесте используется несколько разных наборов тестовых данных,
     // как в AccountsTest и ContactsTest?
     // Ответ Коли: Мы передаём в тест String userName, String password.
-    //Можем передавать Credentials cred, Account testAccount
-    //где Credentials и Account, это объекты классов, которые мы сами придумали и описали.
+    // Можем передавать Credentials cred, Account testAccount
+    // где Credentials и Account, это объекты классов, которые мы сами придумали и описали.
     @DataProvider (name = "loginData")
     public Object[][] loginData(){
         return new Object[][] {
